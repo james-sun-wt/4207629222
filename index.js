ce399ebb35ffd3016387f5390453a8b511a1df8c
James Sun <jamessun92@gmail.com>
2025-02-14 00:16:11 -0500

    use intent deeplink

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const searchParams = new URLSearchParams(window.location.search);
 const deepLink = searchParams.get('af_dp');
 if (deepLink) {
-    window.location.href = decodeURI(deepLink);
+    window.location.href = "intent://m/#Intent;scheme=" + deepLink + ";package=com.fandango.regal.mock;end";
 } else {
     alert('malformed deeplink! missing "af_dp" param');
 }
